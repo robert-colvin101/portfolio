cccb6437348a16adc617c1db00049370a8bd9dc6
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-02-20 22:51:49 +0000

    animation

---
+++ app/javascript/components/scroll_animation.js
@@ -0,0 +1,7 @@
+
+const tl = () => {
+  gsap.to(".main-title-back", {
+  x: 500,
+});
+};
+export { tl };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require 'greensock/ScrollTrigger'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { tl } from '../components/scroll_animation.js';
+import { gsap } from "gsap";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  tl();
+});
